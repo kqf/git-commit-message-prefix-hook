b3d30784cd35ff6597d0ffa55fc8fec50a7fd868
Sasha <q.dev.null@gmail.com>
2020-05-27 19:46:33 +0200

    Add tests for init; Setup the CI

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
+INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 function init-git-commit-message-prefix-hook {
 	[ -d .git ] || {
 		echo "Run this command inside a git repository";
 		return 1
 	}
-	path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-	cp ${path}/hook/prepare-commit-msg .git/hooks/ && echo "Done!"
+
+	cp ${INSTALL_DIR}/hook/prepare-commit-msg .git/hooks/ && echo "Done!"
 }
 
 export -f init-git-commit-message-prefix-hook
---
+++ tests/test_interface.bats
@@ -0,0 +1,24 @@
+#!/usr/bin/env bats
+
+setup() {
+	source init.sh
+
+	export workdir=$(mktemp -d)
+	cd ${workdir}
+}
+
+teardown() {
+	rm -rf ${workdir}
+}
+
+@test 'does nothing for unintialized repository' {
+	run init-git-commit-message-prefix-hook
+	[ "${status}" -eq 1 ]
+}
+
+@test 'initializes hook for existing directory' {
+	git init
+	run init-git-commit-message-prefix-hook
+	echo "Status ${status} and ${output}"
+	[ "${status}" -eq 0 ]
+}
